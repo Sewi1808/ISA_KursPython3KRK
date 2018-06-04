8e7cb80df87324b04b85bbaf4af15816376c10cd
sewi1808 <szalasny.s@gmail.com>
2018-06-04 19:56:52 +0200

    normalize user's input

--- day_3/instrukcje_warunkowe.py
+++ day_3/instrukcje_warunkowe.py
@@ -1,7 +1,9 @@
 ADMIN_USERNAME = 'piotr'
-
 name = input('Podaj imie: ')
+#name lower -> then capitalized
+name_capitalized = name.lower().capitalize()
+
 if name.lower() == ADMIN_USERNAME:
-    print('Hello', name)
+    print('Hello', name_capitalized)
 else:
-    print('Hej', name,  'polkikash?')
+    print('Hej', name_capitalized,  'polkikash?')
