84a3901169564cd22d35c8eadc2ead3c4295c0c2
sebastian <szalasny.s@gmail.com>
2018-06-06 14:55:37 +0200

    8. -> WorkVersion attempt 2

--- practisepython.org/8. rock_paper_scissors.py
+++ practisepython.org/8. rock_paper_scissors.py
@@ -3,16 +3,10 @@ import random
 manual = "First type 2 for game start,\n then Type 1 for rock\n Type 2 for paper\n Type 3 for scissors\n type Q for quit"
 
 while True:
-    usr_command = input("Type:\n 1 - for manual\n 2 - for game start\n")
-    if usr_command == "q":
-        break
-    elif usr_command == 2:
-        p1 = input("Player 1 choose(1 - rock; 2 - paper; 3 - scissors)")
-        p2 = input("Player 2 choose(1 - rock; 2 - paper; 3 - scissors)")
-    elif usr_command == 1:
-        print(manual)
-    else:
-        print("provided wrong command")
-        break
+    pl1=input("Player 1 please provide your choice[r/p/s]: \n")
+    pl2=input("Player 2 please provide your choice[r/p/s]: \n")
 
-print("Won: ")
+    for one in pl1
+        if one =='r' and pl2=='s':
+            print("Player1 won!")
+        elif one == 's' and pl
