efdf6a1b86d89cd1e62109d4b4d7e3f6ab8fed0b
sebastian <szalasny.s@gmail.com>
2018-06-11 17:16:17 +0200

    create and finish guessing_game_one game with random gen

--- practisepython.org/9.guessing_game_one.py
+++ practisepython.org/9.guessing_game_one.py
@@ -1,9 +1,7 @@
 import random
 
-#Guessing game
-
-#get random num
-r_num = random.randint(1,9)
+# get random num
+r_num = random.randint(1, 9)
 # 0 value guess
 guess = 0
 # 0 value count
@@ -16,7 +14,7 @@ while guess != r_num and guess != "exit":
         break
 
     guess = int(guess)
-    count +=1
+    count += 1
 
     if guess < r_num:
         print("your guess was to low! \n :(")
