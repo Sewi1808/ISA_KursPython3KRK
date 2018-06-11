4628d131a18470e7f3eea6b230ea51138a2a8cfb
sewi1808 <szalasny.s@gmail.com>
2018-06-11 18:27:47 +0200

    exercise1 file upload

---
+++ day_5/exercise1.py
@@ -0,0 +1,37 @@
+names = ['pawel', 'michal', 'magda', 'ania', 'paulina']
+
+print(len(names))
+
+first_element = names[0]
+last_element = names[-1]
+
+print(f"{first_element}\n{last_element}")
+
+#dodawanie elemenu na koniec listy
+names.append('sebastian')
+print(names)
+
+names_count = names.count('przemek')
+
+
+
+print(names)
+names.append('przemek')
+names.append('ania'),
+names_count = names.count('ania')
+print(names_count)
+
+name_to_find = 'ania'
+
+foo = names.index(name_to_find)
+
+if name_to_find in names:
+    print(name_to_find, "included in: names")
+else:
+    print("not found")
+
+second_row = names[1:3]
+print(second_row)
+
+for name in names:
+    print(name)
