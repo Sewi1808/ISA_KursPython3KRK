336f359f6e9d83119b53b018faf208a7626ef0ca
sebastian <szalasny.s@gmail.com>
2018-06-21 17:01:36 +0200

    additional exercises, implement difflib.get_close_matches

--- Udemy_Python3_10-apps/First_exercise/first_exercise.py
+++ Udemy_Python3_10-apps/First_exercise/first_exercise.py
@@ -1,21 +1,20 @@
+import json
+import sys
+import difflib
 
 
 def search_in_data():
-    import sys
-    import json
     data = json.load(open("data.json"))
-    user_entry = input('Provide data to find: \n').lower()
-    while True:
-        if user_entry == 'q':
-            print("Program Exit")
-            sys.exit()
-        elif user_entry not in data:
-            print("You provide wrong data, try again")
-            search_in_data()
-        else:
-            print(data[user_entry])
-            search_in_data()
+    user_entry = input('Provide data to find(for quit type "Q": \n').lower()
+    if user_entry == 'q':
+        print("Program Exit")
+        sys.exit()
+    elif user_entry not in data:
+        print("You provide wrong data")
+        print("maybe you meant: \n", difflib.get_close_matches(user_entry, data.keys(), n=1))
+        search_in_data()
+    else:
+        return data[user_entry]
 
 
-search_in_data()
-# print(data['rain'])
+print(search_in_data())
