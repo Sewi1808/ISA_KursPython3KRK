a9259f5a30e19960a04d38619f663b608ffccb66
Sewi <szalasny.s@gmail.com>
2018-07-10 12:49:40 +0200

    class learn

--- day_12/zadanie_domowe_ksiegowosc.py
+++ day_12/zadanie_domowe_ksiegowosc.py
@@ -14,40 +14,46 @@
 # metody obliczajace stanowiska/dzialy na wynagrodzenia i per firma
 
 employee = {"00000":
-                {"Name": "Place_Holder", "Surname": "Place_Holder", "Salary": "Place_Holder",
-                 "Position": "Place_Holder"
-                 }
+                {
+                    "Name": "Place_Holder", "Surname": "Place_Holder",
+                    "Salary": "Place_Holder", "Position": "Place_Holder"}
+
             }
 
 
 class Pracownik:
 
-    # pola klasy
-
-
-
     def __init__(self, imie, nazwisko, pensja, stanowisko):
-        self.__nazwisko = nazwisko
-        self.__pensja = pensja
-        self.__imie = imie
-        self.__stanowisko = stanowisko
+        self.nazwisko = nazwisko
+        self.pensja = pensja
+        self.imie = imie
+        self.stanowisko = stanowisko
 
     @property
     def pracownik_return(self):
-        return str(f"{self.__nazwisko}, {self.__imie}")
+        return str(f"{self.nazwisko}, {self.imie}")
+
+    def emp_append(self):
+        employee[len(employee)] = {"Name": self.imie, "Surname": self.nazwisko,
+                                   "Salary": self.pensja, "Position": self.stanowisko
+                                   }
 
 
 class Position:
     def __init__(self, name, default_salary):
-        self.__name = name
-        self.__default_salary = default_salary
+        self.name = name
+        self.default_salary = default_salary
 
     @property
     def position_return(self):
-        return str(f"{self.__name}, {self.__default_salary}")
+        return str(f"{self.name}, {self.default_salary}")
+
 
 
 Adam = Pracownik("Adam", "Kowalski", 2500, "Kierowca")
 Kierowca = Position("Kierowca", 3200)
 print(Adam.pracownik_return)
 print(Kierowca.position_return)
+for i in employee.items():
+    print(i)
+
