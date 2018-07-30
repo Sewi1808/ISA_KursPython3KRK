bdcf002ff843bb94fa44916a99527c9056a9e9e6
Sewi <szalasny.s@gmail.com>
2018-07-30 20:23:59 +0200

    asd

--- day_12/zadanie_domowe_ksiegowosc.py
+++ day_12/zadanie_domowe_ksiegowosc.py
@@ -13,47 +13,62 @@
 # wylistowanie (oddzielone spacjami, imie nazwisko i wynagrodzenie)
 # metody obliczajace stanowiska/dzialy na wynagrodzenia i per firma
 
-employee = {"00000":
+employee = {0:
                 {
                     "Name": "Place_Holder", "Surname": "Place_Holder",
-                    "Salary": "Place_Holder", "Position": "Place_Holder"}
-
+                    "Salary": "Place_Holder", "Position": "Place_Holder"},
+            1:
+                {
+                    "Name": "Anna", "Surname": "Nowak",
+                    "Salary": 3300, "Position": "Clean Service"}
             }
 
 
-class Pracownik:
-
-    def __init__(self, imie, nazwisko, pensja, stanowisko):
-        self.nazwisko = nazwisko
-        self.pensja = pensja
-        self.imie = imie
-        self.stanowisko = stanowisko
-
-    @property
-    def pracownik_return(self):
-        return str(f"{self.nazwisko}, {self.imie}")
-
-    def emp_append(self):
-        employee[len(employee)] = {"Name": self.imie, "Surname": self.nazwisko,
-                                   "Salary": self.pensja, "Position": self.stanowisko
-                                   }
-
-
 class Position:
-    def __init__(self, name, default_salary):
-        self.name = name
+    def __init__(self, position_name, default_salary):
+        self.position_name = position_name
         self.default_salary = default_salary
 
     @property
     def position_return(self):
-        return str(f"{self.name}, {self.default_salary}")
+        return str(f"{self.position_name}, {self.default_salary}")
+
+
+class Employe(Position):
+
+    def __init__(self, name, surname, salary, position_name):
+        self.surname = surname
+        self.salary = salary
+        self.name = name
+        self.position_name = position_name
+
+    @property
+    def emp_return(self):
+        return str(f"{self.surname} {self.name} {self.salary}")
+
+    def emp_append(self):
+        employee[len(employee)] = {"Name": self.name, "Surname": self.surname,
+                                   "Salary": self.salary, "Position": self.position_name
+                                   }
+
+    def emp_comparision(self):
+        if self.salary == employee[1]["Salary"]:
+            return str("Salary Equal")
+        elif self.salary > employee[1]["Salary"]:
+            return str(f"{self.name} has bigger salary")
+        elif self.salary < employee[1]["Salary"]:
+            return str(f"{self.name} has smaller salary")
+        else:
+            return str(f"bug happend")
 
 
 
-Adam = Pracownik("Adam", "Kowalski", 2500, "Kierowca")
-Kierowca = Position("Kierowca", 3200)
-print(Adam.pracownik_return)
-print(Kierowca.position_return)
+Adam = Employe("Adam", "Kowalski", 2500, "Kierowca")
+#Kierowca = Position("Kierowca", 3200)
+# print(Adam.emp_return)
+#print(Kierowca.position_return)
+Adam.emp_append()
 for i in employee.items():
     print(i)
 
+print(Adam.emp_comparision())
---
+++ test/MyDirectory/ClassTest.py
@@ -0,0 +1,8 @@
+class Test():
+    def __init__(self, arg1, arg2):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.ilosc_kol = 5
+
+x = Test(1,2)
+
